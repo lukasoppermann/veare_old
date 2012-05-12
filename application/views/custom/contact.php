d06d00120317ace1b51a45b22b7ca2bfe854cab9
veare <info@veare.net>
2012-05-12 12:04:50 +1000

    adding new folder to views

---
+++ application/views/custom/contact.php
@@ -0,0 +1,41 @@
+<div id="contact_header">
+	<div class="veare-font">
+		<img src="<?=base_url()?>media/layout/contact_header.png" alt="" />
+	</div>
+</div>
+
+<div class="widget column-two-three" id="email_form">
+	<div class="widget-content">
+		<h3>Get in touch</h3>
+		<p>If you are interested in working with me or have a project I might be interested in, just drop me a line.</p>
+		<form class="form">
+			<div class="email-field">
+				<input id="email" type="email" value="" placeholder="your email address" />
+				<div class="button" id="submit">send</div>				
+			</div>
+			<textarea id="message" value="" placeholder="your message"></textarea>
+		</form>
+	</div>
+</div>
+<div class="widget column-one-three">
+	<div class="widget-content contact">
+		<h3>Contact Information</h3>
+		<span class="company">veare</span>
+		<span class="slogan">web development</span>
+		<a class="mail">info@veare.net</a>
+		<a class="phone">+61 403 437 360</a>
+		<p class="address">	
+			35 Brunswick St<br />
+			VIC 3065 Melbourne,<br />
+			Australia
+		</p>
+		<ul class='social-list'>
+			<li class="twitter icon-small">Twitter</li>
+			<li class="dribbble icon-small">Dribbble</li>
+			<li class="facebook icon-small">Facebook</li>
+			<li class="googleplus icon-small">Google+</li>			
+			<li class="github icon-small">Github</li>
+			<li class="vimeo icon-small">Vimeo</li>
+		</ul>
+	</div>
+</div>
