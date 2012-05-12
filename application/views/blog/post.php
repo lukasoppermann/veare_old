0ff7b4789315adf2586f6b32522468ac43ff1e78
veare <info@veare.net>
2012-05-12 12:04:25 +1000

    working on post page

---
+++ application/views/blog/post.php
@@ -0,0 +1,7 @@
+<div id="post_container">
+	<h1 class="main-headline"><?=$title?></h1>
+	<div class="excerpt"><?=$excerpt?></div>
+	<div class="content"><?=$text?></div>
+	<div class="time"><?=$date?></div>
+	<div class="tags"><span>#programming</span><span>#php</span></div>
+</div>
---
+++ application/views/blog/post_teaser.php
@@ -0,0 +1,10 @@
+<div class="widget">
+	<div class="widget-content">
+		<a class="post-teaser-headline" href="<?=current_url().$permalink?>"><h3><?=$title?></h3><span class="icon open"></span></a>
+		<!-- Teaser Content -->
+		<p><?=variable($excerpt)?></p>
+		<!-- Additional Options -->
+		<div class="time"><?=$date?></div>
+		<div class="tags"><span>#programming</span><span>#php</span></div>
+	</div>
+</div>
