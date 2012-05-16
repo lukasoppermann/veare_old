29d02ee4d82e7f4e41d945358d9465e017672533
veare <info@veare.net>
2012-05-16 22:08:45 +1000

    add fn to build tags

---
+++ application/helpers/content_helper.php
@@ -0,0 +1,58 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * CodeIgniter MY_date Helpers
+ *
+ * @package		CodeIgniter
+ * @subpackage	Helpers
+ * @category	Helpers
+ * @author		Lukas Oppermann - veare.net
+ * @link		http://doc.formandsystem.com/helpers/content
+ */
+/**
+ * Tags - explodes tag string and displays tags
+ *
+ * @access	public
+ * @param	integer
+ * @param	string
+ * @return	int / float
+ */
+/**
+* TODO: 
+* - limit (show depending on ranking)
+* - add link with dynamic link
+* - add params ?
+* - build tag system into db_data	
+*
+*/
+function show_tags( $string = null, $limit = 5, $link = false )
+{
+	// check if tags are set
+	if( trim($string) != null )
+	{
+		// explode string
+		$tags = explode(',',$string);
+		// remove empty elements
+		$tags = array_values(array_filter($tags));
+		// check if limit is set, else set limit
+		if( $limit != null && $limit != false )
+		{
+			$limit = count($tags);
+		}
+		// check if link is set
+		if( $link == false )
+		{
+			// while limit is not reached
+			for( $i = 0; $i < $limit-1; $i++ )
+			{
+				$tag = trim($tags[$i]);
+				$output[] = '<span class="tag-'.$tag.'">#'.$tag.'</span>';
+			}
+		}
+		// return output
+		return implode('',$output);
+	}
+}
+
+
+/* End of file content_helper.php */
+/* Location: ./application/helpers/content_helper.php */
