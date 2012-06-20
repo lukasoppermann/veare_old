69a5ee569ca83b7cc158a03fbf64fe74ab7cd52b
veare <info@veare.net>
2012-06-20 17:55:15 +1000

    working on veare

--- application/config/routes.php
+++ application/config/routes.php
@@ -43,6 +43,9 @@ $route['404_override'] = '';
 // Blog
 $route['(\w{2})/blog'] = 'blog/index';
 $route['(\w{2})/blog/?(.*)?'] = 'blog/entry/$2';
+// Portfolio
+$route['(\w{2})/portfolio'] = 'portfolio';
+$route['(\w{2})/portfolio/?(.*)?'] = 'portfolio/$2';
 // About
 $route['(\w{2})/about'] = 'about';
 $route['(\w{2})/about/?(.*)?'] = 'about/$2';
