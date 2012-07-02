7e80fdcf84220d3678bc81e2d1f90ec00283ef64
veare <info@veare.net>
2012-07-03 08:54:01 +0930

    working on portfolio tag menu

--- application/controllers/portfolio.php
+++ application/controllers/portfolio.php
@@ -20,12 +20,22 @@ class Portfolio extends MY_Controller {
 		{
 			// load into view
 			$entries[] = $this->load->view('portfolio/card', $card, TRUE);
+			// define tag menu
+			$tag_menu = array();
 			// tags
-			if( in_array($tag_menu, ) )
+			foreach( explode(',',$card['tags']) as $tag )
 			{
-				
+				// prepare tag
+				$tag = trim($tag);
+				// add tag to menu if not existing
+				if( !array_key_exists($tag, $tag_menu) )
+				{
+					$tag_menu[$tag] = '<li class="filter-item" data-filter="'.$tag.'">'.$tag.'</li>';
+				}
 			}
 		}
+		// tag menu
+ 		$this->data['tag_menu'] = '<ul id="tag_menu" class="filter-list">'.implode('',$tag_menu).'</ul>';
 		//
 		$this->data['content'] = implode('',$entries);
 		// load view
