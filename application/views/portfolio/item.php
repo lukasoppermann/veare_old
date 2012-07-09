f05504609265c33c5f793e01365b13dc2dd1a01c
veare <info@veare.net>
2012-07-09 11:52:29 +1000

    adding portfolio item page

---
+++ application/views/portfolio/item.php
@@ -0,0 +1,6 @@
+<div id="post_container">
+	<h1 class="main-headline"><?=variable($title)?></h1>
+	<div class="excerpt"><?=variable($excerpt)?></div>
+	<div class="content"><?=variable($text)?></div>
+	<div class="tags"><?=show_tags($tags)?></div>
+</div>
