94ff4705257e6c753ab441aa77ea47e42662c78b
veare <info@veare.net>
2012-08-09 21:57:13 +1000

    trying new way, using responsive grid

---
+++ libs/js/mobile-0.0.1.js
@@ -0,0 +1,11 @@
+// $('body').on('resolutionChange', function(e, resolution){
+// 	if(resolution == 'mobile-portrait')
+// 	{	
+// 		
+// 	}
+// 	else
+// 	{
+// 		alert('land');	
+// 	}
+// });
+// 
