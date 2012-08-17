8be288916159a3251f961ff7a6932ee1e99c7595
veare <info@veare.net>
2012-08-17 23:08:04 +1000

    working on about text

--- application/views/custom/about.php
+++ application/views/custom/about.php
@@ -3,21 +3,30 @@
 		<h2 class="headline-about">Visionary design</h2>
 	</div>
 	<div class="content">
-		<div class="column-left">
-			<p class="excerpt">veare is a Berlin-based interface design studio with a focus on modern branding & intelligent design solutions.</p>
-			<p>An up-to-date concept for your company is important, the intelligent design approach will ensure that your project will be completed to suit your clients needs.
-			</p>
+		<div class="column-left"><p class="excerpt">veare is a Berlin-based interface design studio with a focus on modern branding & intelligent design solutions.</p>
+			<p>veare stands for good design and state-of-the-art development with a focus on user experience.</p>
+			<p>By starting from the point of absolute reduction and adding complexity only where needed a performant and usable experience is the achieved.</p>
 		</div>
-		<div class="column-right"><p>The fast changing world of commerce requires a different approach to branding due to the recent changes including the move towards mobile and the web. Digital media becomes more prominent and important than offline brand experience and therefore the brand has to be integrate deeply into your design to provide a brand experience even without logos and other corporate elements.</p>
+		<div class="column-right"><p>With the consumer in mind the brand experience has to be adapted to the fast changing digital world. A more flexible corporate identity with a strong emphasis on digital is our answer to the problem of modern branding.</p>
+			<p>A passion for usability, simplicity, performance and well delivered content is what makes our projects successful.</p>
+			<p>By building on a network of freelancers veare stays flexible and can adapt to your project, budget and schedule.</p>
 		</div>
 	</div>
 </div>
 <h2 class="column column-five adaptive">Intelligent design</h2>
 <div class="column column-one">
-	Very muhc text
+	Very much text
 </div>
 <section class="column column-three-two">
 	<h3>Vision</h3>
 	<p>Every good project starts with a vision. A glimpse of the future with no restrictions. A starting point of all that could be achieved.</p>
 	<p></p>
 </section>
+
+
+- state-of-art
+- intelligent design 
+- clients needs
+-
+
+The fast changing world of commerce requires a different approach to branding due to the recent changes including the move towards mobile and the web. Digital media becomes more prominent and important than offline brand experience and therefore the brand has to be integrated deeply into your design to provide a brand experience even without logos and other corporate elements.
