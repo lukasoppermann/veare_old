7260d1e6a37718c86b041aff2b2c1b04cf7e6262
veare <info@veare.net>
2012-08-17 15:04:45 +1000

    fixing bugs for contact for both screen and phone

--- libs/js/contact-0.0.1.js
+++ libs/js/contact-0.0.1.js
@@ -5,18 +5,24 @@ $(function()
 	$(window).load( function() {
 		if( !$('body').hasClass('mobile'))
 		{
+			var lat = 52.546167;
+			var lng = 13.4145;
+			var zoom = 15;
+			
+			
 			$('#stage').height($(window).height());
-			var content = '<div class="marker-wrapper">'+$('.veare-contact').show().attr('class', 'veare-contact').wrap('<div class="wrap" />').parents('.wrap').html()+'<div class="shadow"></div></div>';
+			var content = '<div class="marker-wrapper">'+$('.veare-contact').attr('class', 'veare-contact').clone().wrap('<div class="wrap" />').parents('.wrap').html()+'<div class="shadow"></div></div>';
 			// draw map
 			var map, dragevent;
 			var dragfn = function(){
-				map.setCenter(52.546167, 13.415201);
-				map.setZoom(15);	
+				map.setCenter(lat, lng);
+				map.setZoom(zoom);	
 			};
 			map = new GMaps({
 				div: '#veare_map',
-				lat: 52.546167,
-				lng: 13.415201,
+				lat: lat, // lat: 52.546167, - neu:  52.5354,13.43315
+				lng: lng, // lng: 13.415201,
+				
 				disableDefaultUI: true,
 				dragstart: function(){
 					clearTimeout( dragevent );	
@@ -28,37 +34,45 @@ $(function()
 				zoom_changed: function(){
 					clearTimeout( dragevent );
 					dragevent = setTimeout( dragfn, 3000);
+				},
+				tilesloaded: function(){
+					var _marker = $('.marker-wrapper');
+					if(_marker.css('opacity') == 0)
+					{
+						_marker.find('.veare-contact').show();
+						_marker.css({'marginTop':'-400px'}).delay(300).animate({'opacity':'1.0','marginTop':'-160px'}, 300, 'swing').animate({'marginTop':'-183px'}, 300);
+					}
 				}
 			});
 			map.drawOverlay({
-				lat: 52.541167,
-				lng: 13.415201,
+				lat: map.getCenter().lat()-.0054,
+				lng: map.getCenter().lng(),
 				layer: 'overlayLayer',
 				content: content
 			});
 			// debounced resize event (fires once every 100ms)
 			$(window).fs_resize(function(){
-				map.refresh();
-				map.setCenter(map.getCenter().lat(), map.getCenter().lng());
+				if(!$('body').hasClass('mobile'))
+				{
+					$('#stage').height($(window).height());
+					map.refresh();
+					map.setCenter(map.getCenter().lat(), map.getCenter().lng());
+				}
+			});
+			// remove style on resolution change
+			$('body').on('resolutionChange', function(e, resolution){
+				if(resolution == 'mobile-portrait' || resolution == 'mobile-landscape' || resolution == 'mobile')
+				{
+					$('#stage').attr('style','');
+				}
 			});
 		}
 		else
 		{
-			// $('.veare-contact').css({'height':$(document).height()-105,'opacity':0,'display':'block'})
-			// $('.veare-contact').animate({'opacity':1}, 1000);
-			// if($('body').hasClass('landscape'))
-			// {
-			// 	$('.veare-contact').css({'height':$(document).height()-50, 'width':$(document).width()-80,'opacity':0,'display':'block'}).animate({'opacity':1}, 1000);
-			// }
-			//
 			$('body').on('resolutionChange', function(e, resolution){
-				if(resolution == 'mobile-portrait')
+				if(resolution != 'mobile-portrait' && resolution != 'mobile-landscape' && resolution != 'mobile')
 				{
-					// $('.veare-contact').css({'height':$(document).height()-105,'width':'100%','opacity':0,'display':'block'})
-				}
-				else
-				{
-					// $('.veare-contact').css({'height':$(document).height()-50, 'width':$(document).width()-80,'opacity':0,'display':'block'});
+					window.location.reload();
 				}
 			});
 		}
