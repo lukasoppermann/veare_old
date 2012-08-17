08db6d652c821f6cbc24eed8444be9adb3fd417a
veare <info@veare.net>
2012-08-17 23:36:28 +1000

    working on about text

--- application/views/custom/about.php
+++ application/views/custom/about.php
@@ -1,14 +1,14 @@
 <div class="veare-about column column-three">
 	<div class="header">
-		<h2 class="headline-about">Visionary design</h2>
+		<h2 class="headline-about">Visionary Design</h2>
 	</div>
 	<div class="content">
 		<div class="column-left"><p class="excerpt">veare is a Berlin-based interface design studio with a focus on modern branding & intelligent design solutions.</p>
 			<p>veare stands for good design and state-of-the-art development with a focus on user experience.</p>
-			<p>By starting from the point of absolute reduction and adding complexity only where needed a performant and usable experience is the achieved.</p>
+			<p>By starting with a precise vision and from the point of absolute reduction, adding complexity only where needed I create performant and usable experiences.</p>
 		</div>
-		<div class="column-right"><p>With the consumer in mind the brand experience has to be adapted to the fast changing digital world. A more flexible corporate identity with a strong emphasis on digital is our answer to the problem of modern branding.</p>
-			<p>A passion for usability, simplicity, performance and well delivered content is what makes our projects successful.</p>
+		<div class="column-right"><p>With the consumer in mind the brand experience has to be adapted to the fast changing digital world. A more flexible corporate identity with a strong emphasis on digital is my answer to the problem of modern branding.</p>
+			<p>A passion for usability, simplicity, performance and well delivered content is what makes my projects successful.</p>
 			<p>By building on a network of freelancers veare stays flexible and can adapt to your project, budget and schedule.</p>
 		</div>
 	</div>
