48ea528bc425ef1632e74639d5b90eedf345c675
veare <info@veare.net>
2012-08-18 13:50:05 +1000

    working on about liquid layout

--- application/views/custom/about.php
+++ application/views/custom/about.php
@@ -1,16 +1,17 @@
-<div class="veare-about column column-three">
-	<div class="header">
-		<h2 class="headline-about">Visionary Design</h2>
+<article class="article">
+	<div class="type column column-five">About veare</div>
+	<h1 class="main-headline column column-five">Visionary Design</h1>
+	<div class="column column-three">
+		<p class="excerpt">veare is a Berlin-based interface design studio with a focus on modern branding & intelligent design solutions.</p>
 	</div>
-	<div class="content">
-		<div class="column-left"><p class="excerpt">veare is a Berlin-based interface design studio with a focus on modern branding & intelligent design solutions.</p>
-			<p>veare stands for good design and state-of-the-art development with a focus on user experience.</p>
-			<p>By starting with a precise vision and from the point of absolute reduction, adding complexity only where needed I create performant and usable experiences.</p>
-		</div>
-		<div class="column-right"><p>With the consumer in mind the brand experience has to be adapted to the fast changing digital world. A more flexible corporate identity with a strong emphasis on digital is my answer to the problem of modern branding.</p>
-			<p>A passion for usability, simplicity, performance and well delivered content is what makes my projects successful.</p>
-			<p>By building on a network of freelancers veare stays flexible and can adapt to your project, budget and schedule.</p>
-		</div>
+<div class="column-five">	
+	<div class="column column-two">
+		<p>veare stands for good design and state-of-the-art development with a focus on user experience.</p>
+		<p>By starting with a precise vision and from the point of absolute reduction, adding complexity only where needed I create performant and usable products.</p>
+	</div>
+	<div class="column column-two"><p>Not only the product experience but also the brand experience has to be adapted to the fast changing digital world. I create flexible corporate identities with a strong emphasis on digital as a modern branding solution.</p>
+		<p>A passion for usability, simplicity, performance and well delivered content is what makes my projects successful.</p>
+		<p>By building on a network of freelancers veare stays flexible and can adapt to your project, budget and schedule.</p>
 	</div>
 </div>
 <h2 class="column column-five adaptive">Intelligent design</h2>
@@ -23,10 +24,13 @@
 	<p></p>
 </section>
 
-
+</article>
+<!--
 - state-of-art
 - intelligent design 
 - clients needs
 -
 
 The fast changing world of commerce requires a different approach to branding due to the recent changes including the move towards mobile and the web. Digital media becomes more prominent and important than offline brand experience and therefore the brand has to be integrated deeply into your design to provide a brand experience even without logos and other corporate elements.
+
+-->
