08a4b8a7b98b0a59a7757014e4e279b3000121f8
veare <info@veare.net>
2012-08-18 17:37:09 +1000

    working on about text

--- application/views/custom/about.php
+++ application/views/custom/about.php
@@ -1,28 +1,38 @@
 <article class="article">
-	<div class="type column column-five">About veare</div>
-	<h1 class="main-headline column column-five">Visionary Design</h1>
-	<div class="column column-three">
-		<p class="excerpt">veare is a Berlin-based interface design studio with a focus on modern branding & intelligent design solutions.</p>
-	</div>
-<div class="column-five">	
-	<div class="column column-two">
-		<p>veare stands for good design and state-of-the-art development with a focus on user experience.</p>
-		<p>By starting with a precise vision and from the point of absolute reduction, adding complexity only where needed I create performant and usable products.</p>
-		<p>Adapted the brand experience as well as the product experience to the fast changing digital world.</p>
-	</div>
-	<div class="column column-two"><p>By designing flexible corporate identities with a strong emphasis on digital I deliver a modern branding solutions which adapt as media and devices change.</p>
-		<p>A passion for usability, simplicity, performance and well delivered content is what makes my projects successful. Working with a network of freelancers keeps veare flexible to adapt to your project, budget and schedule.</p>
-	</div>
-</div>
-<h2 class="column column-five adaptive">Intelligent design</h2>
-<div class="column column-one">
-	Very much text
-</div>
-<section class="column column-three-two">
-	<h3>Vision</h3>
-	<p>Every good project starts with a vision. A glimpse of the future with no restrictions. A starting point of all that could be achieved.</p>
-	<p></p>
-</section>
+	<section>
+		<div class="type column column-five">About veare</div>
+		<h1 class="main-headline column column-five">Visionary Design</h1>
+		<div class="column column-three">
+			<p class="excerpt">veare is a Berlin-based interface design studio with a focus on modern branding & intelligent design solutions.</p>
+		</div>
+		<div class="column-five">	
+			<div class="column column-two">
+				<p>veare stands for good design and state-of-the-art development with a focus on user experience.</p>
+				<p>By starting with a precise vision and from the point of absolute reduction, adding complexity only where needed I create performant and usable products.</p>
+				<p>Adapted the brand experience as well as the product experience to the fast changing digital world.</p>
+			</div>
+			<div class="column column-two"><p>By designing flexible corporate identities with a strong emphasis on digital I deliver a modern branding solutions which adapt as media and devices change.</p>
+				<p>A passion for usability, simplicity, performance and well delivered content is what makes my projects successful. Working with a network of freelancers keeps veare flexible to adapt to your project, budget and schedule.</p>
+			</div>
+		</div>
+	</section>
+	<section>
+		<h2 class="column column-five adaptive">Intelligent Design</h2>
+		<div class="column column-three-two">
+			<p>By designing flexible corporate identities with a strong emphasis on digital I deliver a modern branding solutions which adapt as media and devices change.</p>
+			<p>A passion for usability, simplicity, performance and well delivered content is what makes my projects successful. Working with a network of freelancers keeps veare flexible to adapt to your project, budget and schedule.</p>
+		</div>
+	</section>
+	<section>
+		<h2 class="column column-five adaptive">Modern Branding</h2>
+		<h3>Vision</h3>
+		<p>Every good project starts with a vision. A glimpse of the future with no restrictions. A starting point of all that could be achieved.</p>
+	</section>
+	<section>
+		<h2 class="column column-five adaptive">Web- & Interface Design</h2>
+		<h3>Vision</h3>
+		<p>Every good project starts with a vision. A glimpse of the future with no restrictions. A starting point of all that could be achieved.</p>
+	</section>
 
 </article>
 <!--
