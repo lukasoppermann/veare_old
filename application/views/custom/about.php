dab93955c86fdd4d152c94fe53c07d4af364ef7d
veare <info@veare.net>
2012-08-19 00:57:55 +1000

    changin about text first section with elisa

--- application/views/custom/about.php
+++ application/views/custom/about.php
@@ -2,36 +2,46 @@
 	<section>
 		<div class="type column column-five">About veare</div>
 		<h1 class="main-headline column column-five open_sans_bold">Visionary Design</h1>
-		<div class="column column-three">
+		<div class="column column-three-three-two">
 			<p class="excerpt">veare is a Berlin-based interface design studio with a focus on modern branding & intelligent design solutions.</p>
 		</div>
 		<div class="column-five">	
 			<div class="column column-two">
 				<p>veare stands for good design and state-of-the-art development with a focus on user experience.</p>
-				<p>By starting with a precise vision and from the point of absolute reduction, adding complexity only where needed I create performant and usable products.</p>
-				<p>Adapted the brand experience as well as the product experience to the fast changing digital world.</p>
+				<p>The brand experience as well as the product experience has to change as media and devices change.</p>
+				<p>By designing flexible corporate identities with a strong emphasis on digital, I deliver a modern branding solutions which can adapt and take advantage of new possibilities.</p>
 			</div>
-			<div class="column column-two"><p>By designing flexible corporate identities with a strong emphasis on digital I deliver a modern branding solutions which adapt as media and devices change.</p>
-				<p>A passion for usability, simplicity, performance and well delivered content is what makes my projects successful. Working with a network of freelancers keeps veare flexible to adapt to your project, budget and schedule.</p>
+			<div class="column column-two">
+				<p>Starting with a precise vision I create performant and usable products, adding complexity only where needed.</p>
+				<p>A passion for usability, simplicity, performance and well delivered content is what makes my projects successful.</p>
+				<p>Working with a network of freelancers keeps veare flexible to adapt to your project, budget and schedule.</p>
 			</div>
 		</div>
 	</section>
-	<section>
-		<h2 class="column column-five open_sans_bold">Intelligent Design</h2>
+	<section class="column-five">
+		<div class="column column-two-two-one"><blockquote class="quote-box">A design approach focusing on the specific not the generic.</blockquote></div>
 		<div class="column column-three-two">
+			<h2 class="open_sans_bold sub-headline">Intelligent Design</h2>
+			<p>By designing flexible corporate identities with a strong emphasis on digital I deliver a modern branding solutions which adapt as media and devices change.</p>
+			<p>A passion for usability, simplicity, performance and well delivered content is what makes my projects successful.</p>
+			<p>Working with a network of freelancers keeps veare flexible to adapt to your project, budget and schedule.</p>
+		</div>
+	</section>
+	<section class="column-five">
+		<div class="column column-three-two">
+			<h2 class="open_sans_bold sub-headline">Modern Branding</h2>
 			<p>By designing flexible corporate identities with a strong emphasis on digital I deliver a modern branding solutions which adapt as media and devices change.</p>
 			<p>A passion for usability, simplicity, performance and well delivered content is what makes my projects successful. Working with a network of freelancers keeps veare flexible to adapt to your project, budget and schedule.</p>
 		</div>
+		<div class="column column-two-two-one"><blockquote class="quote-box">By letting go of total control a corporate identity can succeed in every environment.</blockquote></div>
 	</section>
-	<section>
-		<h2 class="column column-five adaptive open_sans_bold">Modern Branding</h2>
-		<h3 >Vision</h3>
-		<p>Every good project starts with a vision. A glimpse of the future with no restrictions. A starting point of all that could be achieved.</p>
-	</section>
-	<section>
-		<h2 class="column column-five adaptive open_sans_bold">Web- & Interface Design</h2>
-		<h3>Vision</h3>
-		<p>Every good project starts with a vision. A glimpse of the future with no restrictions. A starting point of all that could be achieved.</p>
+	<section class="column-five">
+		<div class="column column-two-two-one"><blockquote class="quote-box">A design approach focusing on the specific not the generic.</blockquote></div>
+		<div class="column column-three-two">
+			<h2 class="open_sans_bold sub-headline">Web- & Interface Design</h2>
+			<p>By designing flexible corporate identities with a strong emphasis on digital I deliver a modern branding solutions which adapt as media and devices change.</p>
+			<p>A passion for usability, simplicity, performance and well delivered content is what makes my projects successful. Working with a network of freelancers keeps veare flexible to adapt to your project, budget and schedule.</p>
+		</div>
 	</section>
 
 </article>
