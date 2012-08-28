1f47b390687faccd1d7c8b3f233b73cc218101f5
veare <info@veare.net>
2012-08-28 10:09:45 +1000

    working on about layout and accing retina sprite

--- application/views/custom/about.php
+++ application/views/custom/about.php
@@ -18,32 +18,44 @@
 			</div>
 		</div>
 	</section>
-	<section class="column-five rearrange section-two" data-rearrange="screen">
+	<section class="column-five rearrange section-two activatable" data-rearrange="screen">
 		<div class="column column-two-one-two quote"><blockquote class="quote-box">A design approach focusing on the specific not the generic.”</blockquote></div>
-		<div class="about-icon intelligent-design"></div>
+		<div class="vision about-icon"><div class="veare-sprite icon"></div></div>
 		<div class="column column-two-three-two content">
-			<h2 class="open_sans_bold sub-headline">Intelligent Design</h2>
-			<p>The worflow I am following is the workflow of intelligent design. The idea is to do informed design based on a design & future vision on the one hand and a data-based user analysis. This means that for each project the focus might be shifted. Some clients want state-of-the-art products that can facinate their modern and upt to date users, while users need to focus more on information and support for legacy browsers, because their users do not have an interest or the technology to support the modern web. By making informed design descisions the users experience will not be lowered for legacy support that is not needed, but will also not exclude users from accessing information.</p>
+			<div class="text">
+				<h2 class="open_sans_bold sub-headline">Vision</h2>
+				<p>Intelligent design means informed decisions based on a future vision and a data-based user analysis. By making informed design decisions based on knowledge about the target users, I can create modern concepts that are designed to suit the user. This means that for each project the focus might be shifted. Some clients want state-of-the-art products that can fascinate their modern and up to date users, while others need to focus more on raw information and support for legacy browsers.</p>
+			</div>
+			<div class="active-bg right"></div>
 		</div>
 	</section>
-	<section class="column-five section-three">
+	<section class="column-five section-three activatable">
+		<div class="about-icon intelligent-design"><div class="veare-sprite icon"></div></div>
+		<div class="column column-two-three-two content">
+			<h2 class="open_sans_bold sub-headline">Intelligent Design</h2>
+			<p>Intelligent design means informed decisions based on a future vision and a data-based user analysis. By making informed design decisions based on knowledge about the target users, I can create modern concepts that are designed to suit the user. This means that for each project the focus might be shifted. Some clients want state-of-the-art products that can fascinate their modern and up to date users, while others need to focus more on raw information and support for legacy browsers.</p>
+		</div>
+		<div class="column column-two-one-two quote"><blockquote class="quote-box">A design approach focusing on the specific not the generic.”</blockquote></div>
+	</section>
+	<section class="column-five section-four activatable rearrange" data-rearrange="screen">
+		<div class="column column-two-one-two quote">
+			<blockquote class="quote-box">By letting go of total control a corporate identity can succeed in every environment.”</blockquote>
+			<div class="about-icon modern-branding"></div>
+		</div>
 		<div class="column column-two-three-two content">
 			<h2 class="open_sans_bold sub-headline">Modern Branding</h2>
 			<p>By designing flexible corporate identities with a strong emphasis on digital I deliver a modern branding solutions which adapt as media and devices change.</p>
 			<p>A passion for usability, simplicity, performance and well delivered content is what makes my projects successful. Working with a network of freelancers keeps veare flexible to adapt to your project, budget and schedule.</p>
 		</div>
-		<div class="column column-two-one-two quote">
-			<blockquote class="quote-box">By letting go of total control a corporate identity can succeed in every environment.”</blockquote>
-			<div class="about-icon modern-branding"></div>
-		</div>
 	</section>
-	<section class="column-five rearrange section-four" data-rearrange="screen">
-		<div class="column column-two-one-two quote"><blockquote class="quote-box">By letting go of total control a corporate identity can succeed in every environment.”</blockquote></div>
-			<div class="about-icon interface-design"></div>
+	<section class="column-five section-five activatable">
 		<div class="column column-two-three-two content">
 			<h2 class="open_sans_bold sub-headline">Web- & Interface Design</h2>
 			<p>By designing flexible corporate identities with a strong emphasis on digital I deliver a modern branding solutions which adapt as media and devices change.</p>
 			<p>A test passion for usability, simplicity, performance and well delivered content is what makes my projects successful. Working with a network of freelancers keeps veare flexible to adapt to your project, budget and schedule.</p>
+		</div>
+		<div class="column column-two-one-two quote"><blockquote class="quote-box">By letting go of total control a corporate identity can succeed in every environment.”</blockquote>
+			<div class="about-icon interface-design"></div>
 		</div>
 	</section>
 
--- libs/js/about-0.0.1.js
+++ libs/js/about-0.0.1.js
@@ -1,3 +1,5 @@
+$(function()
+{
 // on reslution change
 var _body = $('body');
 _body.on('resolutionChange', function(e, resolution){
@@ -23,28 +25,28 @@ _body.on('resolutionChange', function(e, resolution){
 		});
 	}
 });
-// on resize
-_body.fs_resize(function(){
-	if( $('#stage').innerWidth() < $('.main-headline').width() )
-	{
-		alert($('#stage').innerWidth());
-	}
-	if($('.quote-box').width() < 240)
-	{
-		$('.quote-box').css({'fontSize':'110%'});
-	}
-	else
-	{
-		$('.quote-box').css({'fontSize':''});		
-	}
-});
 //------------------------------------
 var _window = $(window);
-// on load
-_window.load( function() {	
-	if( $('#stage').width()-110 < $('.main-headline').width() )
+var window_height;
+var activatables = new Array();
+var activate_areas = new Array();
+// on resize
+_body.fs_resize(function(){
+	window_height = _window.height();
+	$('.activatable').each(function(index){
+		activatables[ index ] = new Array();
+		activatables[ index ].selector = $(this);
+		activatables[ index ].top = Math.round(activatables[index].selector.offset().top);		
+		activatables[ index ].bottom = activatables[ index ].top + activatables[ index ].selector.height();				
+		activate_areas[ index ] = Math.round(activatables[ index ].top);			
+	});
+	if( ($('#stage').width()-$('#sidebar').width()) < $('.main-headline').width() )
 	{
-
+		$('.main-headline').addClass('small-font');
+	}
+	else
+	{
+		$('.main-headline').removeClass('small-font');		
 	}
 	if($('.quote-box').width() < 240)
 	{
@@ -56,35 +58,102 @@ _window.load( function() {
 	}
 });
 //------------------------------------
-
+// on load
+_window.load( function() {	
+	_window.trigger('resize');
+	window_height = _window.height();
+	$('.activatable').each(function(index){
+		activatables[ index ] = new Array();
+		activatables[ index ].selector = $(this);
+		activatables[ index ].top = Math.round(activatables[index].selector.offset().top);		
+		activatables[ index ].bottom = activatables[ index ].top + activatables[ index ].selector.height();				
+		activate_areas[ index ] = Math.round(activatables[ index ].top);			
+	});
+	//
+	if( ($('#stage').width()-$('#sidebar').width()) < $('.main-headline').width() )
+	{
+		$('.main-headline').addClass('small-font');
+	}
+	else
+	{
+		$('.main-headline').removeClass('small-font');		
+	}
+	if($('.quote-box').width() < 240)
+	{
+		$('.quote-box').css({'fontSize':'110%'});
+	}
+	else
+	{
+		$('.quote-box').css({'fontSize':''});		
+	}
+	// create active bgs
+	$('.active-bg.right').each(function(){
+		var _this = $(this);
+		_this.css({'width':$('.active-bg').parents('.column').width()+60+($('#stage').width()-$('.active-bg').position().left), 'height':$('.active-bg').parents('.column').height()+60});
+	});
+	
+	
+});
+//------------------------------------
+var scrollTop = _window.scrollTop();
+var scrollBottom = scrollTop + _window.height();
 _window.on('scroll', function(){
-	var _section_two = $('.section-two');
-	var _section_three = $('.section-three');
-	var _section_four = $('.section-four');		
-	if(!_section_two.hasClass('active') && _window.scrollTop() >= '500' && _window.scrollTop() <= '750')
+	scrollTop = _window.scrollTop();
+	scrollBottom = scrollTop + _window.height();
+	// check for active area
+	$.each(activate_areas, function(index, top)
 	{
-		_section_two.addClass('active');
-	}
-	else if(_section_two.hasClass('active') && (_window.scrollTop() < '500' || _window.scrollTop() > '750'))
-	{
-		_section_two.removeClass('active');		
-	}
+		//
+		// if( scrollTop > top - (window_height/2) && scrollTop+25 < top)
+		if( (activatables[index].bottom >= scrollTop) && (activatables[index].top <= scrollBottom)
+		&& (activatables[index].bottom <= scrollBottom) &&  (activatables[index].top >= scrollTop) )
+		{
+			// $('.activatable.active').removeClass('active');
+			activatables[index].selector.addClass('active');
+		}
+		else
+		{
+			activatables[index].selector.removeClass('active');			
+		}
+	});
+	// function isScrolledIntoView(elem)
+	// {
+	//     var docViewTop = $(window).scrollTop();
+	//     var docViewBottom = docViewTop + $(window).height();
+	// 
+	//     var elemTop = $(elem).offset().top;
+	//     var elemBottom = elemTop + $(elem).height();
+	// 
+	//     return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom)
+	//       && (elemBottom <= docViewBottom) &&  (elemTop >= docViewTop) );
+	// }
 	
-	if(!_section_three.hasClass('active') && _window.scrollTop() >= '750' && _window.scrollTop() <= '1150')
-	{
-		_section_three.addClass('active');
-	}
-	else if(_section_three.hasClass('active') && (_window.scrollTop() < '750' || _window.scrollTop() > '1150'))
-	{
-		_section_three.removeClass('active');		
-	}
-	
-	if(!_section_four.hasClass('active') && _window.scrollTop() >= '1150')
-	{
-		_section_four.addClass('active');
-	}
-	else if(_section_four.hasClass('active') && (_window.scrollTop() < '1150'))
-	{
-		_section_four.removeClass('active');		
-	}
+	// if(!_section_two.hasClass('active') && _window.scrollTop() >= '500' && _window.scrollTop() <= '750')
+	// {
+	// 	_section_two.addClass('active');
+	// }
+	// else if(_section_two.hasClass('active') && (_window.scrollTop() < '500' || _window.scrollTop() > '750'))
+	// {
+	// 	_section_two.removeClass('active');		
+	// }
+	// 
+	// if(!_section_three.hasClass('active') && _window.scrollTop() >= '750' && _window.scrollTop() <= '1150')
+	// {
+	// 	_section_three.addClass('active');
+	// }
+	// else if(_section_three.hasClass('active') && (_window.scrollTop() < '750' || _window.scrollTop() > '1150'))
+	// {
+	// 	_section_three.removeClass('active');		
+	// }
+	// 
+	// if(!_section_four.hasClass('active') && _window.scrollTop() >= '1150')
+	// {
+	// 	_section_four.addClass('active');
+	// }
+	// else if(_section_four.hasClass('active') && (_window.scrollTop() < '1150'))
+	// {
+	// 	_section_four.removeClass('active');		
+	// }
+});
+//
 });
