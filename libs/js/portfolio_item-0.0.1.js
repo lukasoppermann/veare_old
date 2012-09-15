d37573e107d8ac335a312c3b74475d36520ee568
veare <info@veare.net>
2012-09-15 18:30:20 +1000

    equal column height

--- libs/js/portfolio_item-0.0.1.js
+++ libs/js/portfolio_item-0.0.1.js
@@ -46,8 +46,71 @@ $(function(){
 			_window.unbind("scroll", activate_menu);
 		}
 	}
+	// ------------------------------------------------------
+	//
+	var currentTallest = 0,
+	     currentRowStart = 0,
+	     rowDivs = new Array(),
+	     $el,
+	     topPosition = 0;
+			 
+	$('.column').each(function() {
+		 $el = $(this);
+		topPostion = $el.position().top;
+		if (currentRowStart != topPostion) 
+		 {
+			// we just came to a new row.  Set all the heights on the completed row
+			for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+				rowDivs[currentDiv].height(currentTallest);
+			 }
+			 // set the variables for the new row
+			rowDivs.length = 0; // empty the array
+			currentRowStart = topPostion;
+			currentTallest = $el.height();
+			rowDivs.push($el);
+		} 
+		else 
+		{
+			 // another div on the current row.  Add it to the list and check if it's taller
+			 rowDivs.push($el);
+			 currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+		}
+		// do the last row
+		for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+			 rowDivs[currentDiv].height(currentTallest);
+		 }
+	 });
 	// when everything is loaded
 	_window.load(function(){
+	
+		$('.column').height('auto');
+		$('.column').each(function() {
+			$el = $(this);
+			topPostion = $el.position().top;
+			if (currentRowStart != topPostion) 
+			 {
+				// we just came to a new row.  Set all the heights on the completed row
+				for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++)
+				{
+					rowDivs[currentDiv].height(currentTallest);
+				}
+				 // set the variables for the new row
+				rowDivs.length = 0; // empty the array
+				currentRowStart = topPostion;
+				currentTallest = $el.innerHeight();
+				rowDivs.push($el);
+			} 
+			else 
+			{
+				 // another div on the current row.  Add it to the list and check if it's taller
+				 rowDivs.push($el);
+				 currentTallest = Math.max(currentTallest, $el.height());
+			}
+			// do the last row
+			for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+				 rowDivs[currentDiv].height(currentTallest);
+			 }
+		});
 	
 		section_menu();
 		
@@ -79,5 +142,35 @@ $(function(){
 	
 	_window.fs_resize(function(){
 		section_menu();
+		
+		$('.column').height('auto');
+		$('.column').each(function() {
+			$el = $(this);
+			topPostion = $el.position().top;
+			if (currentRowStart != topPostion) 
+			 {
+				// we just came to a new row.  Set all the heights on the completed row
+				for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) 
+				{
+					rowDivs[currentDiv].height(currentTallest);
+				}
+				 // set the variables for the new row
+				rowDivs.length = 0; // empty the array
+				currentRowStart = topPostion;
+				currentTallest = $el.innerHeight();
+				rowDivs.push($el);
+			} 
+			else 
+			{
+				 // another div on the current row.  Add it to the list and check if it's taller
+				 rowDivs.push($el);
+				 currentTallest = Math.max(currentTallest, $el.height());
+			}
+			// do the last row
+			for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+				 rowDivs[currentDiv].height(currentTallest);
+			 }
+		});
+		 
 	});
 });
