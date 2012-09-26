c80be309e687dd2de6711856291905c28ebeb51d
veare <info@veare.net>
2012-09-27 09:20:28 +1000

    slider needs bugfix for sliding with multiple instances

--- libs/js/fs.slides-0.0.1.js
+++ libs/js/fs.slides-0.0.1.js
@@ -231,11 +231,15 @@
 		// Method calling logic
 		if ( methods[method] ) 
 		{
-			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+			return $(this).each(function(){
+				 methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+			});
 		} 
 		else if ( typeof method === 'object' || ! method ) 
 		{
-			return methods.init.apply( this, arguments );
+			return $(this).each(function(){
+				methods.init.apply( this, arguments );
+			});
 		}
 		else
 		{
