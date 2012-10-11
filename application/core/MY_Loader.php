5127a3ebb77c57ff5e7a459bc7b351de7a145628
veare <info@veare.net>
2012-10-11 15:21:21 +1100

    updated MY_Loader to make Driver extendable and replaceable, replacement Driver Lib

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -17,53 +17,101 @@ class MY_Loader extends CI_Loader {
 	}
 	// --------------------------------------------------------------------
 	/**
-     * Database Loader
-     *
-     * @access    public
-     * @param    string    the DB credentials
-     * @param    bool    whether to return the DB object
-     * @param    bool    whether to enable active record (this allows us to override the config setting)
-     * @return    object
-     */
-    function database($params = '', $return = FALSE, $active_record = NULL)
-    {
-        // Grab the super object
-        $CI =& get_instance();
-
-        // Do we even need to load the database class?
-        if (class_exists('CI_DB') AND $return == FALSE AND $active_record == NULL AND isset($CI->db) AND is_object($CI->db)) {
-            return FALSE;
-        }
-
-        // Override DB class
+	* Database Loader
+	*
+	* @access    public
+	* @param    string    the DB credentials
+	* @param    bool    whether to return the DB object
+	* @param    bool    whether to enable active record (this allows us to override the config setting)
+	* @return    object
+	*/
+	function database($params = '', $return = FALSE, $active_record = NULL)
+	{
+		// Grab the super object
+		$CI =& get_instance();
+		// Do we even need to load the database class?
+		if (class_exists('CI_DB') AND $return == FALSE AND $active_record == NULL AND isset($CI->db) AND is_object($CI->db)) {
+			return FALSE;
+		}
+		// Override DB class
 		if( file_exists(APPPATH.'core/DB'.EXT) )
 		{
 			require_once(APPPATH.'core/DB'.EXT);
 		}
 		else
 		{
-	        require_once(BASEPATH.'database/DB'.EXT);
+			require_once(BASEPATH.'database/DB'.EXT);
 		}
 		// Load the DB class
 		$db =& DB($params, $active_record);
 		// extend drivers
-        $my_driver = config_item('subclass_prefix').'DB_'.$db->dbdriver.'_driver';
-        $my_driver_file = APPPATH.'core/'.$my_driver.EXT;
-
-        if (file_exists($my_driver_file)) {
-            require_once($my_driver_file);
-            $db = new $my_driver(get_object_vars($db));
-        }
-
-        if ($return === TRUE) {
-            return $db;
-        }
-
-        // Initialize the db variable.  Needed to prevent
-        // reference errors with some configurations
-        $CI->db = '';
-        $CI->db = $db;
+		$my_driver = config_item('subclass_prefix').'DB_'.$db->dbdriver.'_driver';
+		$my_driver_file = APPPATH.'core/'.$my_driver.EXT;
+		//
+		if (file_exists($my_driver_file)) 
+		{
+			require_once($my_driver_file);
+			$db = new $my_driver(get_object_vars($db));
+		}
+		//
+		if ($return === TRUE) {
+			return $db;
+		}
+		// Initialize the db variable.  Needed to prevent
+		// reference errors with some configurations
+		$CI->db = '';
+		$CI->db = $db;
     }
+	// --------------------------------------------------------------------
+
+	/**
+	 * Extended Driver
+	 *
+	 * Loads a driver library
+	 *
+	 * @param	string	the name of the class
+	 * @param	mixed	the optional parameters
+	 * @param	string	an optional object name
+	 * @return	void
+	 */
+	public function driver($library = '', $params = NULL, $object_name = NULL)
+	{
+		// get prefix
+		$prefix = config_item('subclass_prefix');
+		// load driver lib and extension
+		if ( ! class_exists('CI_Driver_Library') || !class_exists($prefix.'Driver_Library'))
+		{
+			// if not replacement Drivers lib exists load default
+			if ( ! file_exists(APPPATH.'libraries/Driver.php'))
+			{
+				// we aren't instantiating an object here, that'll be done by the Library itself
+				require BASEPATH.'libraries/Driver.php';
+			}
+			else
+			{
+				require APPPATH.'libraries/Driver.php';
+			}
+			
+			// Driver library extending implementation.
+			// If there is a Driver library extending, we call this file.
+			if (file_exists(APPPATH.'libraries/' . $prefix . 'Driver.php'))
+			{
+				require APPPATH.'libraries/' . $prefix . 'Driver.php';
+			}
+		}
+		// if library is not given
+		if ($library == '')
+		{
+			return FALSE;
+		}
+		// We can save the loader some time since Drivers will *always* be in a subfolder,
+		// and typically identically named to the library
+		if ( ! strpos($library, '/'))
+		{
+			$library = ucfirst($library).'/'.$library;
+		}
+		return $this->library($library, $params, $object_name);
+	}
 	// --------------------------------------------------------------------
 	/**
 	 * Load View
