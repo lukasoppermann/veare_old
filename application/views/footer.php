96371d8b80ab6f41a263b23abd02eff185cb77a5
veare <info@veare.net>
2012-10-30 16:45:53 +1100

    hiding invisible stuff from screenreander

--- application/views/footer.php
+++ application/views/footer.php
@@ -3,7 +3,7 @@
 	</div>
 </div>	
 	<div id="footer">
-		<ul class="follow-nav">
+		<ul class="follow-nav" aria-hidden="true">
 			<li class="item"><a class="link twitter" href="https://twitter.com/#!/vearenet" target="_blank"><span class="icon-small twitter"></span></a></li>
 			<li class="item"><a class="link dribbble" href="http://dribbble.com/lukasoppermann" target="_blank"><span class="icon-small dribbble"></span></a></li>
 			<li class="item"><a class="link xing" href="https://www.xing.com/profile/Lukas_Oppermann" target="_blank"><span class="icon-small xing"></span></a></li>
