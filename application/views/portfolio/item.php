0f0a29a6a6f25c4721171475ffa0189e842ca4d6
veare <info@veare.net>
2013-01-12 22:30:29 +1100

    adding new strcuture for portfolio items

--- application/views/portfolio/item.php
+++ application/views/portfolio/item.php
@@ -1,5 +1,9 @@
 <article class="portfolio-item article open_sans">
-	<div class="type column column-five <?=variable($category)?>"><?=(str_replace('-',' ',variable($category)))?></div>
+	<div class="type column-five whitespace <?=variable($category)?>"><?=(str_replace('-',' ',variable($category)))?></div>
 	<?=variable($text)?>
-	<div class="tags"><span class="icon tag"></span><?=show_tags($tags)?></div>	
+	<div class="tags whitespace">
+		<div class="tags-container">
+			<span class="icon tag"></span><?=show_tags($tags, 5, false,', ')?>
+		</div>
+	</div>	
 </article>
