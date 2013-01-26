41a8e9f678fa61d3f52cdce80e8aec33646ea520
veare <info@veare.net>
2013-01-26 09:53:32 +0100

    add remove chache to git

--- github.php
+++ github.php
@@ -1,1 +1,22 @@
 <?php echo `git pull origin master --force && git submodule update --init --recursive 2>&1`;
+// remove chached files
+// ----------------------
+// get css all file names
+$files = glob('libs/css/cache/*');
+foreach($files as $file)
+{ // iterate files
+	if(is_file($file))
+	{
+		unlink($file); // delete file
+	}
+}
+// ----------------------
+// get js all file names
+$files = glob('libs/js/cache/*');
+foreach($files as $file)
+{ // iterate files
+	if(is_file($file))
+	{
+		unlink($file); // delete file
+	}
+}
