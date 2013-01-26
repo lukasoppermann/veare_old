330a51c2bab571d9cf6dcaa527e0f7797936dfc6
veare <info@veare.net>
2013-01-26 10:05:57 +0100

    update github

--- github.php
+++ github.php
@@ -1,22 +1,27 @@
 <?php echo `git pull origin master --force && git submodule update --init --recursive 2>&1`;
-// remove chached files
-// ----------------------
-// get css all file names
-$files = glob('libs/css/cache/*');
-foreach($files as $file)
-{ // iterate files
-	if(is_file($file))
-	{
-		unlink($file); // delete file
+// fetch payload data
+$payload = json_decode($_POST['payload'], TRUE);
+if( strstr(strtolower($payload['commits']['message'],'-cache')) !== FALSE )
+{
+	// remove chached files
+	// ----------------------
+	// get css all file names
+	$files = glob('libs/css/cache/*');
+	foreach($files as $file)
+	{ // iterate files
+		if(is_file($file))
+		{
+			unlink($file); // delete file
+		}
 	}
-}
-// ----------------------
-// get js all file names
-$files = glob('libs/js/cache/*');
-foreach($files as $file)
-{ // iterate files
-	if(is_file($file))
-	{
-		unlink($file); // delete file
+	// ----------------------
+	// get js all file names
+	$files = glob('libs/js/cache/*');
+	foreach($files as $file)
+	{ // iterate files
+		if(is_file($file))
+		{
+			unlink($file); // delete file
+		}
 	}
 }
