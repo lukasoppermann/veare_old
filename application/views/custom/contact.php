8781d91fa1aa556881403528c71c5fb1b0d345af
veare <info@veare.net>
2013-02-04 21:29:22 +0100

    update slogan

--- application/views/custom/contact.php
+++ application/views/custom/contact.php
@@ -1,7 +1,7 @@
 <div id="veare_map"></div>
 <div class="veare-contact column column-two" id="veare_card">
 	<div class="contact-wrapper">
-		<h2 class="company open_sans_bold">veare / intelligent design</h2>
+		<h2 class="company open_sans_bold">veare / visionary design</h2>
 		<div class="contact-details open_sans">
 			<div class="contact-detail">
 				<span class="type">P</span>
