67273119996fe771e13eb8a3cba80e3d4d5aef7c
veare <info@veare.net>
2013-02-07 22:34:24 +0100

    adding google -cache

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -71,7 +71,7 @@ class MY_Controller extends CI_Controller {
 			// add css & js files
 			css_add(array('reset','base','layout','icons','responsiveness','menu', 'gui'));
 			js_add('fs.media_queries, fs.resize, fs.load, fs.history, jquery.fittext.js, base', 'default'); 
-			js_add_lines("CI_BASE = '".base_url()."';", 'default');
+			js_add_lines("CI_BASE = '".base_url()."';var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-7074034-1']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();", 'default');
 			// check for mobile
 			if( mobile() != true )
 			{
