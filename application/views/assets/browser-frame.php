c4c71214f80851b1cf5de63ca124c137bddc47b8
Lukas Oppermann <oppermann.lukas@googlemail.com>
2013-06-27 11:05:55 +0200

    adding images

---
+++ application/views/assets/browser-frame.php
@@ -0,0 +1,10 @@
+<div class="browser-frame border-<?=variable($browser_color,'white')?>">
+	<div class="browser-top border-<?=variable($browser_color,'white')?>">
+		<a class="first circle-link"><div class="circle white border-<?=variable($browser_color,'white')?>"></div></a>
+		<a class="second circle-link"><div class="circle white border-<?=variable($browser_color,'white')?>"></div></a>
+		<a class="third circle-link"><div class="circle white border-<?=variable($browser_color,'white')?>"></div></a>
+	</div>
+	<div class="browser-body">
+		<img src="<?=base_url(TRUE)?>media/<?=variable($image)?>" alt="" style="max-width: 900px; height: auto;"/>
+	</div>
+</div>
