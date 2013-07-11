858536d2e8ed1435807ed16b17560b3cfeb937ad
Lukas Oppermann <oppermann.lukas@googlemail.com>
2013-07-11 20:44:02 +0200

    fix?

--- libs/js/fs.tiles-0.0.1.js
+++ libs/js/fs.tiles-0.0.1.js
@@ -40,8 +40,9 @@
 			{
 				methods.settings.column = column;
 				// get margin
-				methods.settings.margin = methods.settings.items.eq(1+methods.settings.column).position().top-(methods.settings.first.position().top+methods.settings.first.outerHeight(false));
-			console.log(methods.settings.margin);
+				methods.settings.margin = methods.settings.items.eq(1+methods.settings.column).position().top-(methods.settings.first().position().top+methods.settings.first().outerHeight(false));
+			console.log(methods.settings.items.eq(1+methods.settings.column));
+					console.log(methods.settings.first().position().top);
 				// arrange items
 				methods.settings.items.each( function( index, item )
 				{
