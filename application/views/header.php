c10bbe0aaf24c3cd4f9a769403bd8a541aaabaf2
Lukas Oppermann <oppermann.lukas@googlemail.com>
2013-07-14 17:38:15 +0200

    activate css cache

--- application/views/header.php
+++ application/views/header.php
@@ -4,7 +4,7 @@
 	<?
 	echo favicon('favicon');
 	echo meta();
-	echo css('default', FALSE);
+	echo css('default', TRUE);
 	// title 
 	echo title('vea.re | '.variable($meta_title, 'welcome').' | visionary design', 'veare');
 	?>
