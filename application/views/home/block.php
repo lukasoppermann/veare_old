6d6b18714f681e1af6f75615e1baa5fb5b681b52
Lukas Oppermann <oppermann.lukas@googlemail.com>
2013-07-19 20:38:33 +0200

    tst

--- application/views/home/block.php
+++ application/views/home/block.php
@@ -3,7 +3,7 @@
 	<div class="section-content centered introduction">
 		<span class="giant-headline">Hello,</span><br />
 		<h2 class="big-headline">I design interfaces</h2>
-		<h1 class="veare-studio medium-headline">&amp; veare is my design studio.</h1><br />
+		<h1 class="veare-studio medium-headline">&amp; veare is my design studio.</h1>
 	</div>
 	
 </section>
