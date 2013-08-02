429109f0fcd7eae316bbc609ce757424acda9428
Lukas Oppermann <oppermann.lukas@googlemail.com>
2013-08-02 09:06:12 +0200

    update

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -68,6 +68,7 @@ class MY_Controller extends CI_Controller {
 		js_add('fs.resize, fs.textSize, javascript', 'default'); 
         // google analytics
         js_add_lines("var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-7074034-1']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();");
+        
 		// --------------------------------------------------------------------
 		// init menu
 		if ( ! $main_menu = $this->cache->get('menu') )
