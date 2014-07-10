ffa4edbf598bf83318256dd99fb40a86c6584448
Lukas Oppermann <lukas@vea.re>
2014-07-10 16:17:02 +0200

    remove codeigniter

--- application/views/portfolio/index.php
+++
@@ -1,7 +0,0 @@
-<div class="no-flicker section portfolio<?=variable($filtered, '',' ')?>">
-
-	<?=$tag_menu?>
-	
-	<div class="items font-white"><?=$content?></div>
-
-</div>
--- application/views/template.php
+++
@@ -1,3 +0,0 @@
-<?=nl2br($this->load->view('header')); ?>
-<?=$_output; ?>
-<?=nl2br($this->load->view('footer')); ?>
